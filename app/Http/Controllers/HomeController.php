96ef3556023161519315db06b64ae0b4a93a61ea
Imam Satya Wedhatama <14.8182@stis.ac.id>
2020-03-26 07:44:10 +0800

    init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Illuminate\Support\Collection;
+
+use Response;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function proses(Request $request)
+    {
+                $fileName = 'input'.'.'.$request->file->getClientOriginalExtension();
+
+        $request->file->move(public_path(), $fileName);
+         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('template_rekap.xlsx');
+        
+         $spreadSheet = new Spreadsheet();
+          $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('input.csv');
+
+
+          $cellValue = $spreadsheet->getActiveSheet()->getCell('A1')->getValue();
+          $highestRow = $spreadsheet->getActiveSheet()->getHighestRow();
+
+          $cellValue2 = $spreadsheet->getActiveSheet()->getCell('A'.$highestRow)->getValue();
+
+          $a=[];
+   
+          $collection = new Collection();
+    
+          for ($i=2; $i < $highestRow+1 ; $i++) { 
+              $nama = $spreadsheet->getActiveSheet()->getCell('A'.$i)->getValue();
+              
+              $time = $spreadsheet->getActiveSheet()->getCell('H'.$i)->getValue();
+              $time_fix = substr($time, 2, -4);
+              $jam_md = substr($time, 13, -4);
+               $jam =substr($time, 13, -10 ) ;
+              $md = substr($time, 15, -7);
+
+               $convert_jam = (int)$jam+8;
+               
+               if ($convert_jam == 24) {
+                   $convert_jam = 0;
+               }
+               if ($convert_jam > 24) {
+                  
+                   $convert_jam = $convert_jam - 24;
+               }
+               $convert_full = $convert_jam . $md;
+                //  $a[$i-2]=['nama'=>$nama, 'datang'=>$convert_full, 'pulang'=>''];
+            //    $collection->offsetSet('nama', $nama);
+            //    $collection->offsetSet('hadir', $convert_full);
+            $collection->push((object)['nama'=>$nama, 'datang'=>$convert_full, 'pulang'=>'']);
+
+              
+          }
+        //   dd($a->firstWhere('nama', 'Imam Satya Wedhatama'));
+           $collection2 = new Collection();
+        //    dd(count($collection));
+           for ($i=0 ; $i <  count($collection); $i++ ) { 
+            //    dd();
+               $a = $collection->where('nama', $collection[$i]->nama)->values();
+            //    dd(count($a));
+               if (count($a)>=2) {
+                //    dd($a[count($a)-1]);
+                //    dd($a[count($a)-1]);
+                $a[0]->pulang = $a[count($a)-1]->datang;
+                // dd($a[0]);
+               }
+                $collection2->push((object)['nama'=> $a[0]->nama, 'datang'=> $a[0]->datang, 'pulang'=> $a[0]->pulang]);
+           }
+             $unique = $collection2->unique()->values();
+     
+        //    dd($unique[3]->nama);
+
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('template_rekap.xlsx');
+            
+            $worksheet = $spreadsheet->getActiveSheet();
+            
+             $spreadsheet->getActiveSheet()->insertNewRowBefore( 5 ,count($unique));
+            $batas=count($unique)+4;
+            
+
+            for ($i=4; $i < $batas ; $i++) {        
+                //  $spreadsheet->getActiveSheet()->insertNewRowBefore(13+$i, 1);
+               
+                 $worksheet->getCell('A'.$i)->setValue($i-3);
+ 
+               
+                $worksheet->getCell('B'.$i)->setValue($unique[$i-4]->nama);//uraian kegiatan
+                 $worksheet->getCell('C'.$i)->setValue($unique[$i-4]->datang);//uraian kegiatan
+                 $worksheet->getCell('D'.$i)->setValue($unique[$i-4]->pulang);//uraian kegiatan           
+               
+            }
+            
+           $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+           
+          $writer->save('write.xlsx');
+              
+
+        // return response()->json(['success'=>'You have successfully upload file.']);
+        
+    }
+     public function download_excel(){
+           
+           
+             $file="./write.xlsx";
+        return Response::download($file, 'SehatSehatSemuanya'.'.xlsx') ;
+       }
+}
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,165 @@
+<template>
+    <v-app id="inspire">
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col cols="12" sm="8" md="4">
+                        <v-card class="elevation-12">
+                            <v-toolbar color="primary" dark flat>
+                                <v-toolbar-title>Import Absen</v-toolbar-title>
+                                <v-spacer />
+                                <!-- <v-tooltip bottom>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn :href="source" icon large target="_blank" v-on="on">
+                                            <v-icon>mdi-code-tags</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>Source</span>
+                                </v-tooltip>
+                                <v-tooltip right>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn icon large href="https://codepen.io/johnjleider/pen/pMvGQO"
+                                            target="_blank" v-on="on">
+                                            <v-icon>mdi-codepen</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>Codepen</span>
+                                </v-tooltip> -->
+                            </v-toolbar>
+                            <v-card-text>
+                                <!-- <v-form >
+                                    <input  label="Regular" type="file" v-on:change="onFileChange">
+                                    <v-text-field label="Regular" type="file" v-on:change="onImageChange"></v-text-field>
+                                    <q-input @input="val => { file = val[0] }" filled type="file" hint="File Scan" @change="uploadFile" />
+                                </v-form> -->
+
+                                <form @submit="formSubmit" enctype="multipart/form-data">
+
+                                    <!-- <strong>Name:</strong>
+
+                        <input type="text" class="form-control" v-model="name"> -->
+
+                                    <strong>File:</strong>
+
+                                    <input type="file" class="form-control" v-on:change="onFileChange">
+
+
+
+                                    <!-- <button class="btn btn-success">Submit</button> -->
+                                     <v-card-actions>
+                                <v-spacer />
+                                <v-btn color="primary" @click="formSubmit">Proses</v-btn>
+                            </v-card-actions>
+                                </form>
+                            </v-card-text>
+                           
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+    </v-app>
+
+    <!-- <div class="container">
+        <div class="large-12 medium-12 small-12 filezone">
+            <input type="file" id="files" ref="files" multiple v-on:change="handleFiles()"/>
+            <p>
+                Drop your files here <br>or click to search
+            </p>
+        </div>
+
+        <div v-for="(file, key) in files" class="file-listing" :key="file">
+            <img class="preview" v-bind:ref="'preview'+parseInt(key)"/>
+            {{ file.name }}
+            <div class="success-container" v-if="file.id > 0">
+                Success
+            </div>
+            <div class="remove-container" v-else>
+                <a class="remove" v-on:click="removeFile(key)">Remove</a>
+            </div>
+        </div>
+
+        <a class="submit-button" v-on:click="submitFiles()" v-show="files.length > 0">Submit</a>
+    </div> -->
+
+</template>
+
+<script>
+    export default {
+        props: {
+            source: String,
+        },
+        data() {
+            return {
+                file_data: [],
+                files: [],
+                image: '',
+
+                name: '',
+
+                file: '',
+
+                success: ''
+            }
+        },
+        methods: {
+            onFileChange(e) {
+
+                console.log(e.target.files[0]);
+
+                this.file = e.target.files[0];
+
+            },
+
+           async formSubmit(e) {
+                e.preventDefault();
+                let currentObj = this;
+                const configx = {
+                    headers: {
+                        'content-type': 'multipart/form-data',
+                        // 'x-csrf-token': document.querySelectorAll('meta[name=csrf-token]')[0].getAttributeNode('content').value,
+                        //  Authorization: 'Bearer ' + localStorage.getItem('token')
+                    }
+
+                }
+                let formData = new FormData();
+
+                formData.append('file', this.file);
+
+                // let data = new FormData(); 
+                // data.append('file', this.avatar);
+                // data.append('_method', 'put'); // add this
+                // formData.append('_method', 'put'); 
+
+
+                console.log('formData', formData)
+               await axios.post('/proses', formData, configx)
+                    // axios.post('/proses', {formData: this.file})
+
+                    // .then(function (response) {
+                    //     console.log('success')
+                    //     currentObj.success = response.data.success;
+
+                    // })
+
+                    // .catch(function (error) {
+                    //     console.log(error)
+                    //     currentObj.output = error;
+
+                    // });
+
+                    .then(response => {
+                        console.log(response)
+                    }).catch(error => {
+                        console.log(error.response)
+                    })
+                    .then(
+               window.location.href = "/download_excel/"
+                )
+
+            }
+
+        }
+    }
+
+</script>
